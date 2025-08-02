c4eba953fb0b00b9538499d73503b1be66f0c152
Sabin <ruSauron@users.noreply.github.com>
2025-08-02 15:43:25 +0300

    Create AI_TOOLKIT.sh

---
+++ AI_TOOLKIT.sh
@@ -0,0 +1,10 @@
+cd /workspace/
+git clone https://github.com/ostris/ai-toolkit.git
+cd ai-toolkit
+python3 -m venv venv
+source venv/bin/activate
+# install torch first
+pip3 install --no-cache-dir torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
+pip3 install -r requirements.txt
+cd ui
+AI_TOOLKIT_AUTH=myPasS npm run build_and_start
