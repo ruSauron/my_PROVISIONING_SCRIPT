533de495e6ce06edf08ec6ed08562f4f18b0c797
Sabin <ruSauron@users.noreply.github.com>
2025-08-03 00:59:24 +0300

    Update AI_TOOLKIT.sh

--- AI_TOOLKIT.sh
+++ AI_TOOLKIT.sh
@@ -11,7 +11,7 @@ source venv/bin/activate
 # install torch first
 pip3 install --no-cache-dir torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
 pip3 install -r requirements.txt
-pip3 install hf_xet
-nohup python -c 'from huggingface_hub import snapshot_download; snapshot_download(repo_id="rusauron/aitoolkit-8bit-FLUX.1-Kontext-dev")' > /workspace/modeldownload.log 2>&1 &
+#pip3 install hf_xet
+#nohup python -c 'from huggingface_hub import snapshot_download; snapshot_download(repo_id="rusauron/aitoolkit-8bit-FLUX.1-Kontext-dev")' > /workspace/modeldownload.log 2>&1 &
 nohup bash -c "cd /workspace/ai-toolkit/ui && AI_TOOLKIT_AUTH=myPasS npm run build_and_start" > ai-toolkit.log 2>&1 &
 
